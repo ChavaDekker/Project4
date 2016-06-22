5a87d4dd97a5ef2ea20c0147ffa432145fd8af63
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2016-06-22 15:10:37 +0200

    Generated code and checked it

---
+++ Code/Temp code storage/Temp solution for testing/Temp solution for testing/Duodata.cs
@@ -0,0 +1,13 @@
+using System;
+public class Duodata {
+	private string desc;
+	private int amounts;
+
+	public string GetDesc() {
+		return this.desc;
+	}
+	public int GetAmount() {
+		throw new System.Exception("Not implemented");
+	}
+
+}
---
+++ Code/Temp code storage/Temp solution for testing/Temp solution for testing/Piechart.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Piechart : AbstractGraph  {
+	private static ARGB[] circle;
+
+	private ARGB[] Cut(ref object aRGB) {
+		throw new System.Exception("Not implemented");
+	}
+	public override ARGB[] Legenda(ref List<Duodata> duodataList) {
+		throw new System.Exception("Not implemented");
+	}
+	public override ARGB[] Make(ref List<Duodata> duodataList) {
+		throw new System.Exception("Not implemented");
+	}
+
+}
