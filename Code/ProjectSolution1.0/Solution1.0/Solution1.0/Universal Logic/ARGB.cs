f8ae06dc62cb06e406431975f663847b69619db3
Marco den Hollander <marcobenik@msn.com>
2016-06-23 10:54:42 +0200

    add ARGB declarations & accessibility

--- Code/ProjectSolution1.0/Solution1.0/Solution1.0/Universal Logic/ARGB.cs
+++ Code/ProjectSolution1.0/Solution1.0/Solution1.0/Universal Logic/ARGB.cs
@@ -1,12 +1,19 @@
+//Marco den Hollander
+
 using System;
-public class ARGB {
-	private int a;
-	private int r;
-	private int g;
-	private int b;
+public class ARGB
+{
+    public byte a;
+    public byte r;
+    public byte g;
+    public byte b;
 
-	public ARGB(ref byte a, ref byte r, ref byte g, ref byte b) {
-		throw new System.Exception("Not implemented");
-	}
-
+    public ARGB(ref byte a, ref byte r, ref byte g, ref byte b)
+    {
+        this.a = a;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        //throw new System.Exception("Not implemented");
+    }
 }
