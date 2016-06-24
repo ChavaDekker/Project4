e00b000318f4ffc9b98ffe09cd74e92cc1a2833a
MarcHarreman <0894389@hr.nl>
2016-06-24 10:05:08 +0200

    buttonEventArgs

--- Code/ProjectSolution1.0/Solution1.0/Solution1.0/MainActivity.cs
+++ Code/ProjectSolution1.0/Solution1.0/Solution1.0/MainActivity.cs
@@ -22,9 +22,54 @@ namespace Solution1._0
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            Button m1Button = FindViewById<Button>(Resource.Id.M1Button);
+            Button m2Button = FindViewById<Button>(Resource.Id.M2Button);
+            Button m3Button = FindViewById<Button>(Resource.Id.M3Button);
+            Button m4Button = FindViewById<Button>(Resource.Id.M4Button);
+            Button m5Button = FindViewById<Button>(Resource.Id.M5Button);
+            Button m6Button = FindViewById<Button>(Resource.Id.M6Button);
+            Button s1Button = FindViewById<Button>(Resource.Id.S1Button);
+            Button s2Button = FindViewById<Button>(Resource.Id.S2Button);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            m1Button.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+
+            m2Button.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+
+            m3Button.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+
+            m4Button.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+
+            m5Button.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+
+            m6Button.Click += (object sender, EventArgs e) =>
+             {
+
+             };
+
+            s1Button.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+
+            s2Button.Click += (object sender, EventArgs e) =>
+            {
+
+            };
         }
     }
 }
--- Code/ProjectSolution1.0/Solution1.0/Solution1.0/Resources/Resource.Designer.cs
+++ Code/ProjectSolution1.0/Solution1.0/Solution1.0/Resources/Resource.Designer.cs
@@ -61,28 +61,28 @@ namespace Solution1._0
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int M1 = 2131034112;
+			public const int M1Button = 2131034112;
 			
 			// aapt resource value: 0x7f050001
-			public const int M2 = 2131034113;
+			public const int M2Button = 2131034113;
 			
 			// aapt resource value: 0x7f050002
-			public const int M3 = 2131034114;
+			public const int M3Button = 2131034114;
 			
 			// aapt resource value: 0x7f050003
-			public const int M4 = 2131034115;
+			public const int M4Button = 2131034115;
 			
 			// aapt resource value: 0x7f050004
-			public const int M5 = 2131034116;
+			public const int M5Button = 2131034116;
 			
 			// aapt resource value: 0x7f050005
-			public const int M6 = 2131034117;
+			public const int M6Button = 2131034117;
 			
 			// aapt resource value: 0x7f050006
-			public const int S1 = 2131034118;
+			public const int S1Button = 2131034118;
 			
 			// aapt resource value: 0x7f050007
-			public const int S2 = 2131034119;
+			public const int S2Button = 2131034119;
 			
 			static Id()
 			{
