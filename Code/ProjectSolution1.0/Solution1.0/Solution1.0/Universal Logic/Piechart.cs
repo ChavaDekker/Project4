e32aae73171f5648d3b2e020c8bf3d9073623bdd
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2016-06-27 11:44:56 +0200

    Made start of pie chart and new UML

--- Code/ProjectSolution1.0/Solution1.0/Solution1.0/Universal Logic/Piechart.cs
+++ Code/ProjectSolution1.0/Solution1.0/Solution1.0/Universal Logic/Piechart.cs
@@ -17,6 +17,12 @@ public class Piechart : AbstractGraph  {
         ARGB[,] piechart = new ARGB[sizepiechart, sizepiechart];
 
         float lastAngle = 0;
+        double newangle;
+        float percentage;
+        SimpleVector oldVec, newVec;
+        oldVec = new SimpleVector();
+        newVec = new SimpleVector();
+        ARGB colortofill;
 
         int total = 0;
         foreach(Duodata<string, int> i in aDuodataList)
@@ -26,19 +32,31 @@ public class Piechart : AbstractGraph  {
 
         foreach(Duodata<string, int> i in aDuodataList)
         {
+            colortofill = new ARGB(/*random values*/)
             if(i.GetAttr2() > total / 2)
             {
                 throw new System.Exception("Not implemented");
             }
             else
             {
-                for(int x = 0; x<sizepiechart; x++)
+                percentage = (float)i.GetAttr2() / (float)total * 100f;
+                newangle = lastAngle + 2 * Math.PI * percentage;
+                newVec.x = Math.Cos(newangle);
+                newVec.y = Math.Sin(newangle);
+                oldVec.x = Math.Cos(lastAngle);
+                oldVec.y = Math.Sin(lastAngle);
+                for (int x = 0; x<sizepiechart; x++)
                 {
                     for(int y = 0; y<sizepiechart; y++)
                     {
                         if(Math.Sqrt(Math.Pow(x-sizepiechart/2, 2) + Math.Pow(y-sizepiechart/2, 2)) < sizepiechart / 2)
                         {
+                            if(isCounterClockwise(newVec, new SimpleVector(x,y)) && !isCounterClockwise(oldVec, new SimpleVector(x, y)))
+                            {
+                                piechart[x, y] = new ARGB(255, 0, 0, 0);
+                            }
 
+                            
                         }
                         else
                         {
@@ -53,4 +71,33 @@ public class Piechart : AbstractGraph  {
 		throw new System.Exception("Not implemented");
 	}
 
+    public bool isCounterClockwise(SimpleVector line, SimpleVector point)
+    {
+        SimpleVector normal = new SimpleVector(-line.y, line.x);
+        double projection = point.x * line.x + point.y * line.y;
+        if (projection >= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
+
+public class SimpleVector
+{
+    public double x, y;
+    public SimpleVector(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public SimpleVector()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
 }
