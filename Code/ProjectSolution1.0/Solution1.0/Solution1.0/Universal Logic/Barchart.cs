a4f2755edaa695352d163da664d10b7b5c092d87
Marco den Hollander <marcobenik@msn.com>
2016-06-28 13:54:10 +0200

    Finish draw logic for bar chart

--- Code/ProjectSolution1.0/Solution1.0/Solution1.0/Universal Logic/Barchart.cs
+++ Code/ProjectSolution1.0/Solution1.0/Solution1.0/Universal Logic/Barchart.cs
@@ -15,14 +15,11 @@ public class Barchart : AbstractGraph  {
         int GWidth = 500;
         int GIndex = aDuodataList.Count; //amount of bars
         int GBarSize = (GHeight / GIndex) - 10 * GIndex; //individual bar size (vertical)
-        string GBarName; //nametag for specific bar
-        int GBarAmount; //value for specific bar
-        double GBarLength; //Length of bar in %
 
-        ARGB GBarColor;
         Random rnd = new Random();
         ARGB[,] barchart = new ARGB[GHeight, GWidth];
         int total = 0;
+        int iteration = 0;
 
         foreach (Duodata<string, int> i in aDuodataList)
         {
@@ -31,12 +28,13 @@ public class Barchart : AbstractGraph  {
 
         foreach (Duodata<string, int> i in aDuodataList)
         {
+            iteration += 1;
             double percentage = i.GetAttr2() / total * 100;
             for (int x = 0; x < GWidth; x++)
             {
                 for (int y = 0; y < GHeight; y++)
                 {
-                    if (x < percentage * (GWidth / 100) && (y % GHeight / GIndex <= GBarSize))
+                    if (x < percentage * GWidth / 100 && GHeight / GIndex * iteration < y &&  y <= GHeight / GIndex * iteration + GBarSize)
                     {
                         barchart[x, y] = new ARGB(255, 0, 0, 0);
                     }
@@ -59,7 +57,6 @@ public class Barchart : AbstractGraph  {
         //    total += i.GetAttr2();
         //    GCurrentIndex += 1;
         //}
-
         throw new System.Exception("Not implemented");
 	}
 
